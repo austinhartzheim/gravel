978281bdbdce48b9e34a8d87786871dad868113a
Austin Hartzheim <austin@austinhartzheim.me>
2015-06-01 23:18:42 +0000

    Django init.

---
+++ gravel/private_settings.dist.py
@@ -0,0 +1,34 @@
+'''
+Private/Local settings for your application deployment.
+'''
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'PUT YOUR SECRET KEY HERE'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
