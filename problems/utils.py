03d782a70344af7cf99036acbfb5821af2aa5c58
Austin Hartzheim <austin@austinhartzheim.me>
2015-06-02 00:13:28 +0000

    Added code to save submitted problems.

---
+++ problems/utils.py
@@ -0,0 +1,7 @@
+from problems import forms
+
+def create_form_with_request(request):
+    form = forms.ProblemSubmitForm()
+    if request.user.is_authenticated():
+        form.username = request.user.get_username()
+    return form
--- problems/views.py
+++ problems/views.py
@@ -1,19 +1,33 @@
+import django.http
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from problems import models
-from problems import forms
+from problems import models, forms, utils
 
 
 def problem_report(request):
-    form = forms.ProblemSubmitForm()
-    if request.user.is_authenticated():
-        form.username = request.user.get_full_name()
+    form = utils.create_form_with_request(request)
     return render(request, 'problem_report.html', {'form': form})
 
 
 def problem_submit(request):
-    pass
+    if request.method != 'POST':
+        form = utils.create_form_with_request(request)
+        error = 'Error: no form data submitted.'
+        return render(request, 'problem_report.html',
+                      {'form': form, 'error': error})
+
+    form = forms.ProblemSubmitForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'problems/report.html', {'form': form})
+
+    problem = form.save(commit=False)
+    if request.user.is_authenticated():
+        problem.userref = request.user
+        problem.userauthed = True
+    problem.save()
+
+    return django.http.HttpResponseRedirect('/problems/view/%i/' % problem.pk)
 
 
 def problem_view(request, pk):
