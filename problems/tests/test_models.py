b2b718d11f3b97135a6c4272b377e57de1544bcf
Austin Hartzheim <austin@austinhartzheim.me>
2015-06-03 06:30:22 +0000

    Added unit test for Problem.add_response()

---
+++ problems/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from problems import models
+
+
+class TestProblem(TestCase):
+    def test_add_response(self):
+        problem = models.Problem()
+        problem.title = 'test'
+        problem.reference = 'test'
+        problem.description = 'test'
+        problem.save()
+
+        user = User()
+        user.username = 'testuser'
+        user.email = 'test@example.com'
+        user.set_password('test')
+        user.save()
+
+        reply = models.Reply()
+        reply.text = 'test'
+        reply.user = user
+        reply.save()
+
+        problem.add_response(reply)
+
+        self.assertEqual(len(problem.replies()), 1,
+                         'Response not added correctly')
+
