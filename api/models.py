a43a9b50580e04549aca91091c6b7f23b7a2aa6c
Austin Hartzheim <austin@austinhartzheim.me>
2015-06-05 04:13:49 +0000

    Added start of API token request code.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+import time
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class RequestToken(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    token = models.CharField(max_length=64)
+
+    expires = models.DateTimeField()
+
+    @classmethod
+    def build_tokens(cls, user, count=1):
+        '''
+        Generate one or more tokens for the given user.
+        
+        :param `django.contrib.auth.models.User` user: The user that
+          the generated tokens should be valid for.
+        :param int count: The number of tokens to generate.
+        '''
+        i = 0
+        tokens = []
+
+        expiration = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+
+        while i < count:
+            hash = hashlib.sha256(str(time.time()).encode('utf8')).hexdigest()
+            token = RequestToken(user=user, token=hash, expires=expiration)
+            tokens.append(token)
+            i += 1
+
+        cls.objects.bulk_create(tokens)
+
+        return tokens
--- api/views.py
+++ api/views.py
@@ -1,9 +1,39 @@
+import json
+
 import django.http
 from django.http import JsonResponse
 from django.shortcuts import render
 from problems import models
+from api.models import RequestToken
 
 
-def problem_highest_id(request):
+# TODO: Check HMAC validity
+def problem_highest_id(request, user):
     highest_id = models.Problem.objects.order_by('-pk')[0].pk
     return JsonResponse({'id': highest_id})
+
+
+# TODO: check HMAC validity
+def api_get_tokens(request, user):
+    try:
+        data = json.loads(request.body)
+    except ValueError:
+        return django.http.HttpResponseBadRequest('Invalid JSON data')
+
+    count = 1
+    try:
+        if 'count' in data:
+            count = int(data['count'])
+    except ValueError:
+        return django.http.HttpResponseBadRequest('Invalid number of tokens')
+
+    if count > 1024:
+        return django.http.HttpResponseBadRequest('Too many tokens requested')
+    elif count < 0:
+        return django.http.HttpResponseBadRequest('Too few tokens requested')
+
+    tokens = RequestToken.build_tokens(user, count)
+    return JsonResponse({
+        'expires': tokens[0].isoformat(),
+        'tokens': [token.token for token in tokens]
+    })
